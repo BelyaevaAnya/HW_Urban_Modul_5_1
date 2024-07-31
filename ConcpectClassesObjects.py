b6842c6296ea1ca7aa645d9b79a045cc1f1ecf22
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-07-31 12:11:54 +0300

    HW_5_1

--- ConcpectClassesObjects.py
+++ ConcpectClassesObjects.py
@@ -2,15 +2,23 @@ class Human:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        # вызываем при инициализации объекта
+        self.say_info()
 
     def say_info(self):
         print(f'Привет! Меня зовут {self.name}, мне {self.age}')
+
+    def birthday(self):
+        self.age += 1
+        print(f'У меня день рождения!!! Мне теперь {self.age}')
 
 
 den = Human('Денчик', 23)
 max = Human('Максон', 35)
 den.surname = "Попов"
-den.say_info()
-max.say_info()
+den.birthday()
+max.birthday()
+# den.say_info()
+# max.say_info()
 print(id(den), den.name, den.age, den.surname)
 print(id(max), max.name, max.age)
