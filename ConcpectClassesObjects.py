2b7ccd2d2411fc34f1950db281a17c9533e01b4c
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-07-31 12:54:09 +0300

    ConcpectReloadeOperatores

--- ConcpectClassesObjects.py
+++ ConcpectClassesObjects.py
@@ -11,10 +11,18 @@ class Human:
     def birthday(self):
         self.age += 1
         print(f'У меня день рождения!!! Мне теперь {self.age}')
-
+    def __str__(self):
+        return self.name
     def __len__(self):
         return self.age
-    
+    def __lt__(self, other):
+        return self.age < other.age
+    def __gt__(self, other):
+        return self.age > other.age
+    def __eq__(self, other):
+        return self.age == other.age or self.name == other.name
+    def __bool__(self):
+        return bool(self.age)
     def __del__(self):
         print(f'{self.name} ушел')
 
@@ -23,10 +31,25 @@ max = Human('Максон', 35)
 den.surname = "Попов"
 den.birthday()
 max.birthday()
-print(id(den), den.name, den.age, den.surname)
-
-del den
-print(len(max))
+# print(id(den), den.name, den.age, den.surname)
+# print(id(max), max.name, max.age)
+# if den:
+#     den.say_info()
+# print(max)
+# => Привет! Меня зовут Денчик, мне 24
+# print(den < max)
+# print(den > max)
+# print(den == max)
+# max.name = 'Денчик'
+# print(den == max)
+# => True
+# self.age < other.age __lt__
+# True
+# False
+# False
+# a = 6
+# print(den)
+# del den
+# print(len(max))
 # den.say_info()
 # max.say_info()
-print(id(max), max.name, max.age)
