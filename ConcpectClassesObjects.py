10c6ecf502451e2e1c6219efd7d915ff73284c9b
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-07-31 12:01:51 +0300

    Update ConcpectClassesObjects.py

--- ConcpectClassesObjects.py
+++ ConcpectClassesObjects.py
@@ -3,8 +3,14 @@ class Human:
         self.name = name
         self.age = age
 
+    def say_info(self):
+        print(f'Привет! Меня зовут {self.name}, мне {self.age}')
+
 
 den = Human('Денчик', 23)
 max = Human('Максон', 35)
-print(id(den), den.name, den.age)
+den.surname = "Попов"
+den.say_info()
+max.say_info()
+print(id(den), den.name, den.age, den.surname)
 print(id(max), max.name, max.age)
